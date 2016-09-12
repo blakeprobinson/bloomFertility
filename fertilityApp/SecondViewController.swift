fc9df2e5699ddb0691a0482307297fa13069c933
Blake Robinson <blakerobinson@blakes-macbook-pro.local>
2016-09-12 12:09:39 -0400

    Initial Commit

---
+++ fertilityApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  fertilityApp
+//
+//  Created by Blake Robinson on 9/12/16.
+//  Copyright © 2016 Blake Robinson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
