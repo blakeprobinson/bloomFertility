63930143074d15ada2202ea8231b9d5b595955f8
Blake Robinson <blakeprobinson@gmail.com>
2016-11-14 17:37:14 -0500

    created SelectedInput struct to replace several properties on Day

---
+++ fertilityApp/SelectedInput.swift
@@ -0,0 +1,16 @@
+//
+//  SelectedInput.swift
+//  fertilityApp
+//
+//  Created by Blake Robinson on 11/14/16.
+//  Copyright © 2016 Blake Robinson. All rights reserved.
+//
+
+import Foundation
+
+struct SelectedInput {
+    let category:FertilityInput.Category
+    let selection1:FertilityInput.Name
+    let selection2:FertilityInput.Name?
+    
+}
